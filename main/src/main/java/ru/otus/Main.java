3b7b5235b457e2504a8e7f0008e461abade474a8
d.plushchenkov <d.plushchenkov@notamedia.ru>
2024-11-17 16:57:16 +0300

    clean code

--- main/src/main/java/ru/otus/Main.java
+++ main/src/main/java/ru/otus/Main.java
@@ -4,12 +4,6 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-
-        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "info");
-        System.setProperty("org.slf4j.simpleLogger.showDateTime", "true");
-        System.setProperty("org.slf4j.simpleLogger.dateTimeFormat", "yyyy-MM-dd HH:mm:ss.SSS");
-
-
         MyProducer.main(args);
         MyConsumer.consumerFromTopic();
     }
